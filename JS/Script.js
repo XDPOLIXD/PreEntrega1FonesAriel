bad0609e9af986f6b8bab24263d4ad851edf92c4
FonesArielDW <fonesarieldw@gmail.com>
2024-02-27 19:44:08 -0300

    sasa

--- JS/Script.js
+++ JS/Script.js
@@ -1,45 +1,45 @@
 // Definir una función llamada iniciarCalculadora
 function iniciarCalculadora() {
     // Inicializar una variable para controlar el bucle
-    let continuar = true
+    let continuar = true;
 
     // Iniciar un bucle que continuará mientras la variable 'continuar' sea true
     while (continuar) {
         // Obtener la operación del usuario mediante un cuadro de diálogo
-        let operacion = prompt("Ingrese la operación (+, -, *, /):")
+        let operacion = prompt("Ingrese la operación (+, -, *, /):");
 
         // Obtener los operandos del usuario mediante cuadros de diálogo y convertirlos a números de punto flotante
-        let num1 = parseFloat(prompt("Ingrese el primer número:"))
-        let num2 = parseFloat(prompt("Ingrese el segundo número:"))
+        let num1 = parseFloat(prompt("Ingrese el primer número:"));
+        let num2 = parseFloat(prompt("Ingrese el segundo número:"));
 
         // Realizar la operación seleccionada por el usuario y mostrar el resultado
         switch (operacion) {
             case '+':
-                alert(`Resultado: ${num1 + num2}`)
-                break
+                alert(`Resultado: ${num1 + num2}`);
+                break;
             case '-':
-                alert(`Resultado: ${num1 - num2}`)
-                break
+                alert(`Resultado: ${num1 - num2}`);
+                break;
             case '*':
-                alert(`Resultado: ${num1 * num2}`)
-                break
+                alert(`Resultado: ${num1 * num2}`);
+                break;
             case '/':
                 // Verificar si el divisor (num2) no es cero antes de realizar la división
                 if (num2 !== 0) {
-                    alert(`Resultado: ${num1 / num2}`)
+                    alert(`Resultado: ${num1 / num2}`);
                 } else {
-                    alert("Error: No se puede dividir por cero.")
+                    alert("Error: No se puede dividir por cero.");
                 }
-                break
+                break;
             default:
                 // Mostrar un mensaje de error si la operación no es válida
-                alert("Operación no válida.")
+                alert("Operación no válida.");
         }
 
         // Preguntar al usuario si desea realizar otra operación y convertir la respuesta a minúsculas
-        let respuesta = prompt("¿Quieres realizar otra operación? (si/no)").toLowerCase()
+        let respuesta = prompt("¿Quieres realizar otra operación? (si/no)").toLowerCase();
 
         // Actualizar la variable 'continuar' según la respuesta del usuario
-        continuar = respuesta === 'si'
+        continuar = respuesta === 'si';
     }
 }
